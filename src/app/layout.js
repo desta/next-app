f3dcb345e3b871181a124d5c4d76730cd4f1f8b5
desta <desta.s.hamid@gmail.com>
2025-04-16 09:29:38 +0700

    asd

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./providers";
+import "../app/heroui/heroui.css"
+import { Providers } from "./heroui/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,8 +20,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark">
-      <body>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <Providers>
           {children}
         </Providers>
